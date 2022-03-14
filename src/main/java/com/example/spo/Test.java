5e77ce0ab227e0033d1217e637fe567c3fbcb2bb
Ilia <asd120200@gmail.com>
2022-03-14 18:37:32 +0400

    .

--- src/main/java/com/example/spo/Test.java
+++ src/main/java/com/example/spo/Test.java
@@ -1,14 +1,6 @@
 package com.example.spo;
 
 import com.example.spo.jni.JNI;
-import com.example.spo.model.Element;
-import com.example.spo.utils.BinaryFileUtil;
-import com.example.spo.utils.CsvFileUtil;
-import com.example.spo.utils.FileUtil;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
