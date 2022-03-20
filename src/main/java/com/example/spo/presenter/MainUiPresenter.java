4e41718a4597dc9141b514602cba958857ae5bf9
Ilia <asd120200@gmail.com>
2022-03-20 18:16:34 +0400

    fix

--- src/main/java/com/example/spo/presenter/MainUiPresenter.java
+++ src/main/java/com/example/spo/presenter/MainUiPresenter.java
@@ -1,4 +1,4 @@
-package com.example.spo;
+package com.example.spo.presenter;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.spo {
 
     opens com.example.spo to javafx.fxml;
     exports com.example.spo;
+    exports com.example.spo.presenter;
+    opens com.example.spo.presenter to javafx.fxml;
 }
