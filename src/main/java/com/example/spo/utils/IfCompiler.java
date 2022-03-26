9be23007d239f2bf61d7057198ba7ab32dc89b89
Ilia <asd120200@gmail.com>
2022-03-26 12:46:58 +0400

    fix IfCompiler

--- src/main/java/com/example/spo/utils/IfCompiler.java
+++ src/main/java/com/example/spo/utils/IfCompiler.java
@@ -6,21 +6,41 @@ import net.openhft.compiler.CompilerUtils;
 public class IfCompiler {
     public static int execute(MyCode code) throws Exception{
         String className = "com.example.spo.utils.MyClass2";
-        StringBuilder javaCode = new StringBuilder();
-        javaCode.append("package com.example.spo.utils;");
-        javaCode.append("public class MyClass2 implements CodeSample {");
-        javaCode.append("public int run() {");
-        javaCode.append("int k=-1;");
-        javaCode.append("if (").append(code.getCondition()).append("){");
-        javaCode.append(code.getBody());
-        javaCode.append(" k = 1;}");
-        if(code.getBodyElse()!=null){
-            javaCode.append("else{k=0;").append(code.getBodyElse()).append("}");
-        }
-        javaCode.append("return k;}}");
+//        StringBuilder javaCode = new StringBuilder();
+//        javaCode.append("package com.example.spo.utils;");
+//        javaCode.append("public class MyClass2 implements CodeSample {");
+//        javaCode.append("public int run() {");
+//        javaCode.append("int k=-1;");
+//        javaCode.append("if (").append(code.getCondition()).append("){");
+//        javaCode.append(code.getBody());
+//        javaCode.append(" k = 1;}");
+//        if(code.getBodyElse()!=null){
+//            javaCode.append("else{k=0;").append(code.getBodyElse()).append("}");
+//        }
+//        javaCode.append("return k;}}");
+
+        String javaCode = "package com.example.spo.utils;\n" +
+                "\n" +
+                "import java.io.*;\n" +
+                "\n" +
+                "public class MyClass2 implements Runnable\n" +
+                "{\n" +
+                "    public void run() {\n" +
+                "        int k=-1;if (5<9){int b; k = 1;}\n" +
+                "        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(new File(\"data.txt\")))) {\n" +
+                "            dos.writeInt(k);\n" +
+                "        } catch (FileNotFoundException e) {\n" +
+                "            e.printStackTrace();\n" +
+                "        } catch (IOException e) {\n" +
+                "            e.printStackTrace();\n" +
+                "        }\n" +
+                "    }\n" +
+                "}\n";
 
         Class aClass = CompilerUtils.CACHED_COMPILER.loadFromJava(className, javaCode.toString());
-        CodeSample runner = (CodeSample) aClass.newInstance();
-        return runner.run();
+        Runnable runner = (Runnable) aClass.newInstance();
+        runner.run();
+
+        return 0;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,14 @@ module com.example.spo {
     requires javafx.fxml;
     requires lombok;
     requires compiler;
+    requires org.slf4j;
 
     opens com.example.spo to javafx.fxml;
     exports com.example.spo;
     exports com.example.spo.presenter;
     opens com.example.spo.presenter to javafx.fxml;
+    exports com.example.spo.utils;
+    opens com.example.spo.utils to javafx.fxml;
+    opens com.example.spo.model to javafx.fxml;
+    exports com.example.spo.model;
 }
